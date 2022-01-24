2db6b1b204d6f8a63ec09c152d464c444ea40cb6
Gabriel Barbosa Da Conceição <gabriel.bc.dev@hotmail.com>
2022-01-24 10:59:59 -0300

    Unic commit

---
+++ Produto.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cource_Revisao_Const_Prop
+{
+    internal class Produto
+    {
+        // Atributos da classe Produto;
+        private string _nome { get; set; }
+        private double _preco { get; set; }
+        private int _quantidade { get; set; }
+
+        // Construtor (PADRÃO) vazio;
+        public Produto()
+        {
+
+        }
+        // Construtor (PERSONALIZADO) com 3 atributos;
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+        // Construtor (PERSONALIZADO) com 2 atributos;
+        public Produto(string nome, double preco)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = 0; // Não se faz nescessario adicionar esse atributo,  por convenção valores numéricos recebem 0;
+        }
+        // Propriedade get e set para o atributo Nome, com uma condição para validação;
+        public string Nome
+        {
+            get { return _nome; }
+            set {
+                if (value != null && value.Length > 1) // Se for maior que 2 caracteres e não for Nulo;
+                    _nome = value; 
+            }
+        }
+        // Propriedade get e set para o atributo Preco;
+        public double Preco
+        {
+            get { return _preco; }
+            set { _preco = value; }
+        }
+        // Propriedade get e set para o atributo Quantidade;
+        public int Quantidade
+        {
+            get { return _quantidade; }
+            set { _quantidade = value; }
+        }
+        // Metodo que retorna o estoque total em valores;
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+        // Metodo para adicionar produtos ao estoque;
+        public void AdicionarProdutos(int quantidade)
+        {
+            //  Quantidade += quantidade; OU > Quantidade = Quantidade + quantidade;
+            _quantidade = _quantidade + quantidade;
+        }
+        // Metodo para remover produtos do estoque;
+        public void RemoverProdutos(int quantidade)
+        {
+            //  Quantidade -= quantidade; OU > Quantidade = Quantidade - quantidade;
+            _quantidade = _quantidade - quantidade;
+        }
+
+        // Um metodo personalizado da classe para mostrar os dados na tela;
+        public override string ToString()
+        {
+            return $"Nome: {_nome.ToUpper()}, Preço: {_preco.ToString("F2", CultureInfo.InvariantCulture)}, {_quantidade} " +
+                $"unidades, Valor total em estoque: {ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using Cource_Revisao_Const_Prop;
+using System;
+using System.Globalization;
+namespace Course_Revisao_Const_Prop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Já instânciei meu construtor personalizado com um nome padrão (Produto, Preco(0.0) e Quantidade(0);
+            // CONSTRUTOR PERSONALIZADO 1º OPÇÃO;
+            //Produto p = new Produto("Produto", 0.0, 0);
+
+            //Console.WriteLine("\nDados do produto : " + p);
+
+            // Setar dados dos produtos: Nome, Preco e quantidade;
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome:");
+            string nome = Console.ReadLine().ToUpper();
+            Console.Write("Preço:");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, quantidade); //(CONSTRUTOR PERSONALIZADO 2º OPÇÃO)
+            // Aqui poderia instânciar meu construtor personalizado, conforme vou recebendo os atributos no conlsole;
+
+            // Construtor padrão vazio / como sobrecarga de construtores
+            Produto p2 = new Produto();
+
+            // Costrutor personalizado com 3 atributos ja instânciados (Nome: "Carro",Preco: 35000.00, 20);
+            Produto p3 = new Produto();
+            p3.Nome ="Carro";
+            p3.Preco = 35000.00;
+            p3.Quantidade = 20;
+            
+            
+            Console.WriteLine("\nDados do produto : " + p);
+
+            // Setar adicionar e remover produtos
+            Console.Write("\nDigite o numero de produtos a ser adicionado no estoque:");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine("\nDados do produto: " + p);
+
+            Console.Write("\nDigite o numero de produtos a ser removido do estoque:");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            // Mostra na tela os dados do produto do objeto da classe p;
+            Console.WriteLine("\nDados atualizados: " + p);
+
+            // Mostra na tela os dados do produto do objeto da classe p3;
+            Console.WriteLine("\nDados atualizados: " + p3);
+        }
+    }
+}
